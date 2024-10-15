db92e60e3cee9f5e9966fafbe248c7cf89e98e1b
Makarasok24 <165671537+Makarasok24@users.noreply.github.com>
2024-10-15 17:02:13 +0700

    init

---
+++ posSystem/src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="w-full bg-yellow-400 h-[100px] flex items-center justify-center">
+      <p>POS System</p>
+    </div>
+  )
+}
+
+export default App
